331ad4fabb45634fe0876447e576965c50023bde
Raphael Lima <oraphadev@gmail.com>
2022-11-18 14:36:00 -0300

    fix(develop): adjust migrate

--- migrate.js
+++ migrate.js
@@ -3,21 +3,19 @@ const mongoConn = require('./connections/mongodb');
 const collectionPedidos = require('./data/collections/pedidos.json');
 const collectionSetores = require('./data/collections/setor.json');
 
-async function migrate() {
-  await mongoConn.connect();
-  const db = mongoConn.db('lab_vi');
-
-  const pedidos = db.collection('pedidos');
-  const tvShows = db.collection('setores');
-
-  await pedidos.insertMany(collectionPedidos);
-  await tvShows.insertMany(collectionSetores);
-}
-
-try {
-  await migrate();
-} catch (err) {
-  console.log(err);
-} finally {
-  mongoConn.close();
-}
+(async function migrate() {
+  try {
+    await mongoConn.connect();
+    const db = mongoConn.db('lab_vi');
+  
+    const pedidos = db.collection('pedidos');
+    const tvShows = db.collection('setores');
+  
+    await pedidos.insertMany(collectionPedidos);
+    await tvShows.insertMany(collectionSetores);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    mongoConn.close();
+  }
+})();
